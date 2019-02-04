7df2f6b543b9ad5912f8f30b1e8ab8f423a1b340
Kenbxitn <kenbxitn@gmail.com>
2019-02-05 00:57:08 +0300

    remove secret key from settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*nlm!$*++w#&&z+%(zqp-l---*!t3o3i6x3$swjp=chbf%kni@'
+SECRET_KEY = 'import from secret.py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
